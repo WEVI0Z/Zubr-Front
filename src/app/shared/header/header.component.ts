cb1a799c1d89afa87fcc7d7b7fb0e34956ba972a
quocanh.trinh <quocanh.trinh@intexsoft.by>
2024-01-05 15:50:22 +0300

    header design done

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ParticlesConfig } from './particles-config';
+
+declare let particlesJS: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
 
 }
