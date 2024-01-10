662ae6dd376db12c8ff64271b4f1ed9d25ec79a4
quocanh.trinh <quocanh.trinh@intexsoft.by>
2024-01-10 15:58:34 +0300

    created media, partners and sphere components

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import {MainRoutingModule} from "./main-routing.module";
 import {SharedModule} from "../shared/shared.module";
+import { SphereComponent } from './sphere/sphere.component';
+import { MediaComponent } from './media/media.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    SphereComponent,
+    MediaComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
