5b25ca3e334bd704858a971109ab2e608c8a1ca3
quocanh.trinh <quocanh.trinh@intexsoft.by>
2024-01-22 13:09:01 +0300

    Main sphere component refactor

--- src/app/main/sphere/sphere.component.ts
+++ src/app/main/sphere/sphere.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Sphere} from "../../sphere-list/interface/sphere";
+import {SphereService} from "../../sphere-list/service/sphere.service";
 
 @Component({
   selector: 'app-sphere',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sphere.component.scss']
 })
 export class SphereComponent {
+  protected sphereList: Sphere[] = this.sphereService.sphereList
+    .sort((a, b) => 1 * (Math.floor(Math.random() * 2) == 1 ? 1 : -1))
+    .slice(0, 3);
 
+  constructor(private sphereService: SphereService) {
+    console.log(this.sphereList)
+  }
 }
