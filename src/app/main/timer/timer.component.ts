368cd59f18f7e07bd3e351e12035faae5b7c29d5
alexey.garkaviy <wevioz@mail.ru>
2024-03-09 12:19:27 +0300

    Timer fix

--- src/app/main/timer/timer.component.ts
+++ src/app/main/timer/timer.component.ts
@@ -20,6 +20,9 @@ export class TimerComponent implements OnInit{
   protected minutesLeft: number = 0;
   protected secondsLeft: number = 0;
   protected dayString: string = '';
+  protected hourString: string = '';
+  protected minuteString: string = '';
+  protected secondString: string = '';
   protected milliseconds: number = 0;
   protected ifTimerRelevant: boolean = new Date() < TARGET_DATE;
 
@@ -53,8 +56,45 @@ export class TimerComponent implements OnInit{
     }
 
     if ((this.daysLeft % 10 >= 5 && this.daysLeft % 10 <= 9) ||
-      this.daysLeft === 11 || this.daysLeft === 10 ||this.daysLeft === 0) {
+      this.daysLeft === 11 || this.daysLeft === 10 ||this.daysLeft % 10 === 0 ||
+      this.daysLeft === 12 || this.daysLeft === 13 || this.daysLeft === 14) {
       this.dayString = "дней";
+    }
+
+    if (this.hoursLeft % 10 === 1) {
+      this.hourString = "час"
+    } else if (this.hoursLeft % 10 >= 2 && this.hoursLeft % 10 <= 4) {
+      this.hourString = "часа"
+    }
+
+    if ((this.hoursLeft % 10 >= 5 && this.hoursLeft % 10 <= 9) ||
+      this.hoursLeft === 11 || this.hoursLeft === 10 ||this.hoursLeft === 0 ||
+      this.hoursLeft === 12 || this.hoursLeft === 13 || this.hoursLeft === 14) {
+      this.hourString = "часов";
+    }
+
+    if (this.minutesLeft % 10 === 1) {
+      this.minuteString = "минута"
+    } else if (this.minutesLeft % 10 >= 2 && this.minutesLeft % 10 <= 4) {
+      this.minuteString = "минуты"
+    }
+
+    if ((this.minutesLeft % 10 >= 5 && this.minutesLeft % 10 <= 9) ||
+      this.minutesLeft === 11 || this.minutesLeft === 10 ||this.minutesLeft === 0 ||
+      this.minutesLeft === 12 || this.minutesLeft === 13 || this.minutesLeft === 14) {
+      this.minuteString = "минут";
+    }
+
+    if (this.secondsLeft % 10 === 1) {
+      this.secondString = "секунда"
+    } else if (this.secondsLeft % 10 >= 2 && this.secondsLeft % 10 <= 4) {
+      this.secondString = "секунды"
+    }
+
+    if ((this.secondsLeft % 10 >= 5 && this.secondsLeft % 10 <= 9) ||
+      this.secondsLeft === 11 || this.secondsLeft === 10 ||this.secondsLeft === 0 ||
+      this.secondsLeft === 12 || this.secondsLeft === 13 || this.secondsLeft === 14) {
+      this.secondString = "секунд";
     }
   }
 }
