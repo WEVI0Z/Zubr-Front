9ee36a4a3fc0fb0e0d413300e73499bcb7fd5e80
Egoryk0806 <egor.potapnko.2003@gmail.com>
2024-03-26 15:57:40 +0300

    stop animation added

--- src/app/main/partners/partners.component.ts
+++ src/app/main/partners/partners.component.ts
@@ -11,33 +11,53 @@ export class PartnersComponent implements AfterViewInit{
   @ViewChild("mainWrapper", {read: ElementRef}) mainWrapper!: ElementRef;
   offset: number = 0;
   multiplier: number = -1;
+  imageChangeInterval: any;
+  isImageChangePaused: boolean = false;
 
   ngAfterViewInit(): void {
-    setInterval(() => {this.changeImage()}, 1500);
+    this.imageChangeInterval = setInterval(() => {
+      this.changeImage();
+    }, 2250);
   }
 
   changeImage(): void {
-    const element: HTMLElement = this.partnerList.nativeElement;
-    const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
-    const mainWrapper: HTMLElement = this.mainWrapper.nativeElement;
-    const mainWidth: number = mainWrapper.clientWidth;
-    const elementWidth: number = element.clientWidth;
-    const wrapperWidth: number = wrapper.clientWidth;
-    const offsetPerScroll: number = elementWidth * 0.1;
+    if (!this.isImageChangePaused) {
+      const element: HTMLElement = this.partnerList.nativeElement;
+      const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
+      const mainWrapper: HTMLElement = this.mainWrapper.nativeElement;
+      const mainWidth: number = mainWrapper.clientWidth;
+      const elementWidth: number = element.clientWidth;
+      const wrapperWidth: number = wrapper.clientWidth;
+      const offsetPerScroll: number = elementWidth * 0.1;
 
-    if (mainWidth <= 778) {
-      element.style.transform = ``;
-      return;
+      if (mainWidth <= 778) {
+        element.style.transform = ``;
+        return;
+      }
+
+      if (this.offset * -1 >= elementWidth - wrapperWidth) {
+        this.multiplier = 1
+      } else if (this.offset >= 0) {
+        this.multiplier = -1;
+      }
+
+      this.offset += offsetPerScroll * this.multiplier;
+
+      element.style.transform = `translateX(${this.offset}px)`
     }
+    
+  }
 
-    if (this.offset * -1 >= elementWidth - wrapperWidth) {
-      this.multiplier = 1
-    } else if (this.offset >= 0) {
-      this.multiplier = -1;
-    }
 
-    this.offset += offsetPerScroll * this.multiplier;
+  stopImageChange(): void {
+    this.isImageChangePaused = true;
+    clearInterval(this.imageChangeInterval);
+  }
 
-    element.style.transform = `translateX(${this.offset}px)`
+  resumeImageChange(): void {
+    this.isImageChangePaused = false;
+    this.imageChangeInterval = setInterval(() => {
+      this.changeImage();
+    }, 2250);
   }
 }
