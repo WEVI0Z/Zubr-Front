d5862b2746be11e434d654910cbdc6d272622a4b
Egoryk0806 <egor.potapnko.2003@gmail.com>
2024-03-28 15:59:38 +0300

    Mouse scroll, scroll button

--- src/app/main/partners/partners.component.ts
+++ src/app/main/partners/partners.component.ts
@@ -12,7 +12,8 @@ export class PartnersComponent implements AfterViewInit{
   offset: number = 0;
   multiplier: number = -1;
   imageChangeInterval: any;
-  isImageChangePaused: boolean = false;
+  isRButtonHidden: boolean = false;
+  isLButtonHidden: boolean = false;
 
   ngAfterViewInit(): void {
     this.imageChangeInterval = setInterval(() => {
@@ -21,43 +22,92 @@ export class PartnersComponent implements AfterViewInit{
   }
 
   changeImage(): void {
-    if (!this.isImageChangePaused) {
-      const element: HTMLElement = this.partnerList.nativeElement;
-      const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
-      const mainWrapper: HTMLElement = this.mainWrapper.nativeElement;
-      const mainWidth: number = mainWrapper.clientWidth;
-      const elementWidth: number = element.clientWidth;
-      const wrapperWidth: number = wrapper.clientWidth;
-      const offsetPerScroll: number = elementWidth * 0.1;
+    const element: HTMLElement = this.partnerList.nativeElement;
+    const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
+    const mainWrapper: HTMLElement = this.mainWrapper.nativeElement;
+    const mainWidth: number = mainWrapper.clientWidth;
+    const elementWidth: number = element.clientWidth;
+    const wrapperWidth: number = wrapper.clientWidth;
+    const offsetPerScroll: number = elementWidth * 0.1;
+    this.isRButtonHidden = false;
+    this.isLButtonHidden = false;
 
-      if (mainWidth <= 778) {
-        element.style.transform = ``;
-        return;
-      }
+    if (mainWidth <= 778) {
+      element.style.transform = ``;
+      return;
+    }
 
-      if (this.offset * -1 >= elementWidth - wrapperWidth) {
-        this.multiplier = 1
-      } else if (this.offset >= 0) {
-        this.multiplier = -1;
-      }
+    if (this.offset * -1 >= elementWidth - wrapperWidth) {
+      this.multiplier = 1
+    } else if (this.offset >= 0) {
+      this.multiplier = -1;
+    }
 
-      this.offset += offsetPerScroll * this.multiplier;
+    this.offset += offsetPerScroll * this.multiplier;
 
-      element.style.transform = `translateX(${this.offset}px)`
+    element.style.transform = `translateX(${this.offset}px)`
+
+    if (this.offset * -1 >= elementWidth - wrapperWidth) {
+      this.isRButtonHidden = true;
+    } else if (this.offset >= 0) {
+      this.isLButtonHidden = true;
     }
     
   }
 
+  previosImage(): void {
+    const element: HTMLElement = this.partnerList.nativeElement;
+    const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
+    const elementWidth: number = element.clientWidth;
+    const wrapperWidth: number = wrapper.clientWidth;
+
+    this.multiplier = 1;
+    this.changeImage();
+    clearInterval(this.imageChangeInterval);
+
+    if (this.offset * -1 >= elementWidth - wrapperWidth) {
+      this.isRButtonHidden = true;
+    } else if (this.offset >= 0) {
+      this.isLButtonHidden = true;
+    }
+  }
+
+  nextImage(): void {
+    const element: HTMLElement = this.partnerList.nativeElement;
+    const wrapper: HTMLElement = this.partnerWrapper.nativeElement;
+    const elementWidth: number = element.clientWidth;
+    const wrapperWidth: number = wrapper.clientWidth;
+
+    this.multiplier = -1;
+    this.changeImage();
+    clearInterval(this.imageChangeInterval);
+
+    if (this.offset * -1 >= elementWidth - wrapperWidth) {
+      this.isRButtonHidden = true;
+    } else if (this.offset >= 0) {
+      this.isLButtonHidden = true;
+    }
+  }
 
   stopImageChange(): void {
-    this.isImageChangePaused = true;
     clearInterval(this.imageChangeInterval);
   }
 
   resumeImageChange(): void {
-    this.isImageChangePaused = false;
     this.imageChangeInterval = setInterval(() => {
       this.changeImage();
     }, 2250);
   }
+
+  handleMouseWheel(event: any) {
+    event.preventDefault();
+    clearInterval(this.imageChangeInterval);
+
+    if (event.deltaY > 0) {
+      this.nextImage(); // Вызовите функцию для прокрутки вниз
+    } else if (event.deltaY < 0) {
+      this.previosImage(); // Вызовите функцию для прокрутки вверх
+    }
+   }
+
 }
