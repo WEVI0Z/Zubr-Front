36154be0f2192ff4f22ff08678bbdb7e98306e58
alexey.garkaviy <wevioz@mail.ru>
2024-04-06 23:59:19 +0300

    fix: scroll to top fix

---
+++ src/app/shared/scroll-top/scroll-top.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScrollTopComponent } from './scroll-top.component';
+
+describe('ScrollTopComponent', () => {
+  let component: ScrollTopComponent;
+  let fixture: ComponentFixture<ScrollTopComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ScrollTopComponent]
+    });
+    fixture = TestBed.createComponent(ScrollTopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
